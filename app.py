43e1a79cd768f34b811aad18e97b8eae085e4a1a
telecaster15 <62332004+telecaster15@users.noreply.github.com>
2020-07-29 10:33:03 +0900

    200729

--- app.py
+++ app.py
@@ -7,38 +7,85 @@ app.secret_key = "super secret key"
 
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+
+    return render_template('index_t.html')
+
+
+@app.route("/index_js")
+def index_js():
+    render_template("/js/scripts.js")
+
+
+@app.route('/join')
+def join():
+    render_template('join.html')
 
 
 
-@app.route('/search', methods=['GET'])
-def search():
-    depart_list=search_proc()
-    return render_template('search.html',depart_list=depart_list)
 
 
-@app.route('/search_proc', methods=['GET'])
-def search_proc():
+# @app.route('/depart_search', methods=['GET','POST'])
+# def depart_search():
+#     if (request.method=='POST'):
+#         depart = request.form['depart']
+#         if (depart==None) :
+#             depart = '   -   '
+#         else:
+#             url = 'http://www.juso.go.kr/addrlink/addrLinkApi.do'
+#             params = {'confmKey':'devU01TX0FVVEgyMDIwMDYxOTE4MDA0ODEwOTg4NTY=',
+#                       'currentPage':'1',
+#                       'countPerPage':'10',
+#                       'keyword': depart,
+#                       'resultType':'json'
+#                       }
+#
+#             response = requests.get(url,params=params)
+#             location_data = response.json()
+#             juso = location_data['results']['juso']
+#             if juso == None:
+#                 flash('해당 결과가 존재하지 않습니다.')
+#                 return redirect('/')
+#             else:
+#                 d_jibunAddr_list=[]
+#                 for i in range(len(juso)):
+#                     jibunAddr = juso[i]['jibunAddr']
+#                     d_jibunAddr_list.append(jibunAddr)
+#     else:
+#         depart = "  -  "
+#     return render_template('index.html',d_jibunAddr_list=d_jibunAddr_list)
 
-    depart = request.args.get('depart')
-    app_key = 'KakaoAK '+ 'ae56cb54a2506b6fb7ba4972ee6e1f58'
-    url = 'https://dapi.kakao.com/v2/local/search/address.json'
-    params = {'query':depart,
-              'page':'',
-              'AddressSize':''
-              }
-    headers = {'Authorization':app_key}
-    response = requests.get(url,params=params, headers=headers)
-
-    map_data = response.json()
-    map_doc = map_data['documents'][0]
-    x = map_doc['x']
-    y = map_doc['y']
-    location=[x,y]
-    return location
+# @app.route('/arrive_search', methods=['GET','POST'])
+# def arrive_search():
+#     if (request.method=='POST'):
+#
+#         if (arrive==None) :
+#             depart = '   -   '
+#         else:
+#             url = 'http://www.juso.go.kr/addrlink/addrLinkApi.do'
+#             params = {'confmKey':'devU01TX0FVVEgyMDIwMDYxOTE4MDA0ODEwOTg4NTY=',
+#                       'currentPage':'1',
+#                       'countPerPage':'10',
+#                       'keyword': arrive,
+#                       'resultType':'json'
+#                       }
+#
+#             response = requests.get(url,params=params)
+#             location_data = response.json()
+#             juso = location_data['results']['juso']
+#             if juso == None:
+#                 flash('해당 결과가 존재하지 않습니다.')
+#                 return redirect('/')
+#             else:
+#                 a_jibunAddr_list=[]
+#                 for i in range(len(juso)):
+#                     jibunAddr = juso[i]['jibunAddr']
+#                     a_jibunAddr_list.append(jibunAddr)
+#     else:
+#         depart = "  -  "
+#     return render_template('index.html',a_jibunAddr_list=a_jibunAddr_list)
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(port=1212)
+    app.run(port=5000)
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import requests
 import json
+from flask import Flask, request, render_template, redirect, url_for, flash
 
 url = 'http://www.juso.go.kr/addrlink/addrLinkApi.do'
 params = {'confmKey':'devU01TX0FVVEgyMDIwMDYxOTE4MDA0ODEwOTg4NTY=',
           'currentPage':'1',
-          'countPerPage':'5',
-          'keyword':'신도림역',
+          'countPerPage':'10',
+          'keyword': '서울역',
           'resultType':'json'
           }
 
@@ -13,9 +14,4 @@ response = requests.get(url,params=params)
 location_data = response.json()
 juso = location_data['results']['juso']
 
-jibunAddr_list=[]
-for i in range(len(juso)):
-    jibunAddr = juso[i]['jibunAddr']
-    print(jibunAddr)
-
-
+print(juso[1])
